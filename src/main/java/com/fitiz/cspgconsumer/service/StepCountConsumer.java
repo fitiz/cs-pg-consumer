d04fee29468ecea7db0120ea67647c0935c9c7ef
burakemreozer <burakemreozer@gmail.com>
2024-02-04 04:54:55 +0100

    adjust logs

--- src/main/java/com/fitiz/cspgconsumer/service/StepCountConsumer.java
+++ src/main/java/com/fitiz/cspgconsumer/service/StepCountConsumer.java
@@ -21,13 +21,13 @@ public class StepCountConsumer {
             properties = {"spring.json.value.default.type=com.fitiz.cspgconsumer.model.StepCountUpdateData"})
     public void stepCountPgConsumer(ConsumerRecord<String, StepCountUpdateData> record) {
         var stepCountUpdateData = record.value();
-        log.info("Step count consumed, user: {}, step count: {}", stepCountUpdateData.username(), stepCountUpdateData.steps());
+        log.info("Step count consumed, [user: {}, step count: {}]", stepCountUpdateData.username(), stepCountUpdateData.steps());
         boolean updatedStepCount = leaderboardPgRepository.updateStepCount(stepCountUpdateData.userId(),
                 stepCountUpdateData.steps());
         if (!updatedStepCount) {
             log.error("Failed to update step count for user: {}", stepCountUpdateData.username());
             return;
         }
-        log.info("Step count saved to DB , user: {}", stepCountUpdateData.username());
+        log.info("Step count saved to DB...");
     }
 }
